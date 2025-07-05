4f60f706627649b2dda79f624ca0ea2466eed1fb
Md Bayzid Bostami <88899794+Bayzid2@users.noreply.github.com>
2025-07-05 14:39:12 +0600

    Update qa_quiz.cy.js

--- cypress/e2e/qa_quiz.cy.js
+++ cypress/e2e/qa_quiz.cy.js
@@ -9,39 +9,39 @@ describe('meldCX qa quiz', () => {
     
     it('Verify login page is visible',() =>{
         //Assert titte of login page
-        cy.get('.sc-bdVaJa').should('have.text','qa.code-quiz.dev')
+        cy.get('.sc-bdVaJa').should('have.text','qa.code-quiz.dev');
         //Assert login page
         cy.get('.sc-ifAKCX > div').should('have.text','If you do not have an account, contact an admin').and('be.visible');
     })
 
   it('Login button visible',() =>{
     //Assert login button
-    cy.get('.sc-bZQynM').should('be.visible').and('have.text','LOGIN')
+    cy.get('.sc-bZQynM').should('be.visible').and('have.text','LOGIN');
   })
 
   it('Try to login with invalid username',() =>{
-    cy.get(usernameField).type('SomeUser_namewee')
-    cy.get(passwordField).type('TopSecret1234!')
-    cy.get(loginBtn).click()
+    cy.get(usernameField).type('SomeUser_namewee');
+    cy.get(passwordField).type('TopSecret1234!');
+    cy.get(loginBtn).click();
 })
 
  it('Try to login with invalid password',() =>{
-    cy.get(usernameField).type('SomeUser_namewee')
-    cy.get(passwordField).type('TopSecret1234!')
-    cy.get(loginBtn).click()
+    cy.get(usernameField).type('SomeUser_namewee');
+    cy.get(passwordField).type('TopSecret1234!');
+    cy.get(loginBtn).click();
 })
 
 let name ='.sc-htpNat.ilgFCs';
-let logout = '.sc-bxivhb.fqCnAP'
+let logout = '.sc-bxivhb.fqCnAP';
 
 it('Login with valid credentials',() =>{
     //Login
-    cy.get(usernameField).type('SomeUser_name')
-    cy.get(passwordField).type('TopSecret1234!')
-    cy.get(loginBtn).click()
+    cy.get(usernameField).type('SomeUser_name');
+    cy.get(passwordField).type('TopSecret1234!');
+    cy.get(loginBtn).click();
 
     //Assert Title
-    cy.get('.sc-bdVaJa > div').should('have.text','Hello SomeName').and('be.visible')
+    cy.get('.sc-bdVaJa > div').should('have.text','Hello SomeName').and('be.visible');
 
      // Assert name value
     cy.get(name).contains('div','Name').siblings('div').should('have.text','SomeName');
@@ -67,10 +67,10 @@ it('Login with dummytree user',() =>{
 
     //Assert title
     cy.get('.sc-bdVaJa').should('have.text','Hello undefined').and('be.visible');
-
+    
     // Assert name value
     cy.get(name).contains('div','Name').siblings('div').should('have.text','');
-
+    
      // Assert favourite fruit
     cy.contains('Favourite Fruit').next().should('have.text', 'Mango');
 
@@ -81,9 +81,18 @@ it('Login with dummytree user',() =>{
     cy.contains('Favourite Number').next().should('have.text','The last prime number');
 
     //Assert logout button and click
-    //cy.get(logout).should('have.text','LOGOUT').click();
+    cy.get(logout).should('have.text','LOGOUT').click();
 
 })
-
+    
+it('After logging in, reload the page to check whether the user remains logged in',() =>{
+      //Login
+    cy.get(usernameField).type('SomeUser_name');
+    cy.get(passwordField).type('TopSecret1234!');
+    cy.get(loginBtn).click();
+    cy.reload();
+     //Assert Title
+    cy.get('.sc-bdVaJa > div').should('have.text','Hello SomeName').and('be.visible');
+})
 });
 
